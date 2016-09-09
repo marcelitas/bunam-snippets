3c34f3855e139b22dc11f5f9fec637a48d50239b
terracing <omicrontester@outlook.com>
2016-09-09 10:18:20 -0500

    Patches in tools menu snippet

--- herramientas/js/scripts.js
+++ herramientas/js/scripts.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
 	$('#tools').click(function(){
 		$('.container').toggleClass("open-sidebar");
+		$(this).toggleClass("open-sidebar");
+		$('#sidebar').toggleClass("show-sidebar");
 	});
-	/*$("[data-toggle]").click(function() {
-		var toggle_el = $(this).data("toggle");
-		$(toggle_el).toggleClass("open-sidebar");
-	});*/
 
 	$('#sidebar ul li a').click(function(event){
 		event.preventDefault();
